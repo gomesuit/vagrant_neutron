cb811b83e7f425f25f856cb5088faf2e6df57113
gomesuit <gomesuit@gmail.com>
2016-02-25 20:11:01 +0900

    check network

--- provision.sh
+++ provision.sh
@@ -7,3 +7,37 @@ yum install -y openvswitch
 systemctl start openvswitch
 systemctl status openvswitch
 ovs-vsctl show
+
+## Network Namespaceの作成
+# ip netns add qrouter1
+## veth pearの作成
+# ip link add qr-veth1 type veth peer name qr-peer1
+# ip link set qr-veth1 netns qrouter1
+
+## スイッチの作成及び接続
+# ovs-vsctl add-br br-int
+# ovs-vsctl add-port br-int qr-peer1
+
+## Network Namespaceの作成
+# ip netns add qvm1
+## veth pearの作成
+# ip link add vm-veth1 type veth peer name vm-peer1
+# ip link set vm-veth1 netns qvm1
+
+## スイッチの接続
+# ovs-vsctl add-port br-int vm-peer1
+
+## state up
+# ip netns exec qrouter1 ip addr add 10.0.0.1/21 dev qr-veth1
+# ip netns exec qvm1 ip addr add 10.0.0.3/21 dev vm-veth1
+# ip link set qr-peer1 up
+# ip link set vm-peer1 up
+# ip link set br-int up
+# ip link set ovs-system up
+# ip netns exec qvm1 ip link set vm-veth1 up
+# ip netns exec qvm1 ip link set lo up
+# ip netns exec qrouter1 ip link set qr-veth1 up
+# ip netns exec qrouter1 ip link set lo up
+
+# check network
+# ip netns exec qvm1 ping 10.0.0.1
